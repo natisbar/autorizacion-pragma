403684790ef4e1401f3aacd2dd2a743d644f0df3
Natalia Barbosa <48846184+natisbar@users.noreply.github.com>
2025-08-26 17:08:25 -0500

    HU1: se ajusta test que estaba levantando servidor real y entraba en conflicto con app levantada

--- applications/app-service/src/test/java/co/com/pragma/MainApplicationTest.java
+++ applications/app-service/src/test/java/co/com/pragma/MainApplicationTest.java
@@ -4,16 +4,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureWebTestClient
 class MainApplicationTest {
 
     @Test
-    void contextLoads() {
-    }
-
-    @Test
     void mainMethodRuns() {
-        MainApplication.main(new String[] {});
+        assertDoesNotThrow(() -> {
+            MainApplication.main(new String[] { "--server.port=0" });
+        });
     }
 }
